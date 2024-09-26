075a19093053ff07898b64eeabb91f6ca60e2f93
Hari Kiran <125038766+HariKiran30@users.noreply.github.com>
2024-09-26 11:30:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.mobile-nav').classList.toggle('active');
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("name").textContent = "I'm Hari Kiran";
+});
+
+
+
